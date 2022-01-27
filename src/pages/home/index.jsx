346fb81fec080e0a6002b3cac3b2e9f3293ee071
Gabriel Butarelo <gabrielbutarelo16@gmail.com>
2022-01-26 22:48:19 -0300

    first-commit

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Oi</p>
+        <p>Oi</p>
+      </>
+    )
+  }
+}
+
+export default Home;
